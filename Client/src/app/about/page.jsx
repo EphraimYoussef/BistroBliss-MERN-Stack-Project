f20abce889636394e50f3b16cf0745bdd58a1416
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-10-01 18:52:14 +0300

    about page

---
+++ Client/src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-6xl font-bold min-h-screen flex justify-center items-center'>
+			About Us
+		</div>
+  )
+}
+
+export default page
