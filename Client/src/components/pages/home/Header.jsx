3375551915cd13114720fa4fd49682162d6c8737
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-10-01 15:02:07 +0300

    Header V0.1.1

--- Client/src/components/pages/home/Header.jsx
+++ Client/src/components/pages/home/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<div className='h-dvh flex justify-center items-center bg-[url("/header.png")] bg-cover'>
+		<div className='h-dvh flex justify-center items-center bg-[url("/header.png")] bg-cover bg-no-repeat'>
 			<div className='flex flex-col justify-center items-center gap-5'>
 				<img src="/text.svg" alt="" />
 				<div className='flex justify-around items-center w-2/3'>
