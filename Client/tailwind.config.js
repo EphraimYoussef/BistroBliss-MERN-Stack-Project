1fa7139aefa31eb143f9dd2a507415215255e5d7
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-10-01 14:58:50 +0300

    tailwind

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,78 +5,79 @@ module.exports = {
 	"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 	"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 	"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-],
-theme: {
-	container: {
-		center: 'true',
-		padding: '2rem',
-		screens: {
-			'2xl': '1400px'
-		}
-	},
-	extend: {
-		backgroundImage: {
-			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+	"./src/atoms/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: 'true',
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
 		},
-		borderRadius: {
-			lg: 'var(--radius)',
-			md: 'calc(var(--radius) - 2px)',
-			sm: 'calc(var(--radius) - 4px)'
-		},
-		colors: {
-			primary: {
-				DEFAULT: "#AD343E",
-				dark: "#B36300",
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
 			},
-			secondary: {
-			DEFAULT: "#F5F4F4",
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
 			},
-			light: {
-			DEFAULT: "#FFFEFE",
-			},
-			background: 'hsl(var(--background))',
-			foreground: 'hsl(var(--foreground))',
-			card: {
-				DEFAULT: 'hsl(var(--card))',
-				foreground: 'hsl(var(--card-foreground))'
-			},
-			popover: {
-				DEFAULT: 'hsl(var(--popover))',
-				foreground: 'hsl(var(--popover-foreground))'
-			},
-			// primary: {
-			// 	DEFAULT: 'hsl(var(--primary))',
-			// 	foreground: 'hsl(var(--primary-foreground))'
-			// },
-			// secondary: {
-			// 	DEFAULT: 'hsl(var(--secondary))',
-			// 	foreground: 'hsl(var(--secondary-foreground))'
-			// },
-			muted: {
-				DEFAULT: 'hsl(var(--muted))',
-				foreground: 'hsl(var(--muted-foreground))'
-			},
-			accent: {
-				DEFAULT: 'hsl(var(--accent))',
-				foreground: 'hsl(var(--accent-foreground))'
-			},
-			destructive: {
-				DEFAULT: 'hsl(var(--destructive))',
-				foreground: 'hsl(var(--destructive-foreground))'
-			},
-			border: 'hsl(var(--border))',
-			input: 'hsl(var(--input))',
-			ring: 'hsl(var(--ring))',
-			chart: {
-				'1': 'hsl(var(--chart-1))',
-				'2': 'hsl(var(--chart-2))',
-				'3': 'hsl(var(--chart-3))',
-				'4': 'hsl(var(--chart-4))',
-				'5': 'hsl(var(--chart-5))'
+			colors: {
+				primary: {
+					DEFAULT: "#AD343E",
+					dark: "#952d36",
+				},
+				secondary: {
+				DEFAULT: "#F5F4F4",
+				},
+				light: {
+				DEFAULT: "#FFFEFE",
+				},
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				// primary: {
+				// 	DEFAULT: 'hsl(var(--primary))',
+				// 	foreground: 'hsl(var(--primary-foreground))'
+				// },
+				// secondary: {
+				// 	DEFAULT: 'hsl(var(--secondary))',
+				// 	foreground: 'hsl(var(--secondary-foreground))'
+				// },
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
 			}
 		}
-	}
-},
-plugins: [require("tailwindcss-animate")],
+	},
+	plugins: [require("tailwindcss-animate")],
 };
