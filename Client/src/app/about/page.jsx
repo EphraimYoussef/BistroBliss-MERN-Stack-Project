1d7326dbb82b40d8c666447cb77958be3bef8428
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-10-06 21:05:53 +0300

    about metadata

--- Client/src/app/about/page.jsx
+++ Client/src/app/about/page.jsx
@@ -4,6 +4,9 @@ import Features from '@/components/pages/about us/Features'
 import Testimonials from '@/components/pages/about us/Testimonials'
 import React from 'react'
 
+export const metadata = {
+  title:"About us",
+}
 const page = () => {
   return (
     <div>
