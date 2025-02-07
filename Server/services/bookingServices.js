3b9f05f8a8630724250387b65f0b20ffe22290e2
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-08 01:00:07 +0200

    Update Services

--- Server/services/bookingServices.js
+++ Server/services/bookingServices.js
@@ -1,9 +1,39 @@
 const bookingRepo = require('../repos/bookingRepo');
 
-const createBooking = async (booking) => {}
+const createBooking = async (booking) => {
+    try{
+        const newBooking = await bookingRepo.createBooking(booking);
+        return newBooking;
+    }catch(err){
+        throw new Error(err.message);
+    }
+}
 
-const updateBookingStatus = async (id, status) => {}
+const updateBookingStatus = async (id, status) => {
+    try{
+        const newookingStatus = await bookingRepo.updateBookingStatus(id, status);
+        return newookingStatus;
+    }catch(err){
+        throw new Error(err.message);
+    }
+}
 
-const getAllPendingBookings = async () => {}
+const getAllPendingBookings = async () => {
+    try {
+        const pendingBookings = await bookingRepo.getAllPendingBookings();
+        return pendingBookings;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
 
-module.exports = { createBooking, updateBookingStatus }
+const getAllUserBookings = async (id) => {
+    try {
+        const userBookings = await bookingRepo.getAllUserBookings(id);
+        return userBookings;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+module.exports = { createBooking, updateBookingStatus, getAllPendingBookings, getAllUserBookings }
--- Server/services/userServices.js
+++ Server/services/userServices.js
@@ -1,31 +1,46 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const config = require('../config/config');
-const customerRepo = require('../repos/customerRepo');
-const UnauthorizedError = require('../Errors/UnauthorizedError');
+const userRepo = require('../repos/userRepo');
+// const UnauthorizedError = require('../Errors/UnauthorizedError');
+const bookingService = require('./bookingServices');
+const mealService = require('./mealServices');
 
 
 const signUp = async (user) => {
     try {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        user.password = hashedPassword;
-        const newUser = await customerRepo.createUser(user);
-        return newUser;
+        const foundUser = await userRepo.findUserByEmail(user.email);
+        if (foundUser) {
+            throw new Error('User already exists');
+        }
+        const newUser = await userRepo.createUser(user);
+        const token = jwt.sign({
+            id:newUser._id,
+            isAdmin: newUser.isAdmin,
+            email: newUser.email, 
+            firstName: newUser.firstName,
+            lastName: newUser.lastName
+        }
+        , config.jwt.secret, {expiresIn: '1d'});
+        return {newUser, token};
     } catch (error) {
         throw new Error(error.message);
     }
 }
 
-const login = async (user) => {
+
+const login = async (email, password) => {
     try {
-        const foundUser = await customerRepo.findUserByEmail(user.email);
+        const foundUser = await userRepo.findUserByEmail(email);
         if (!foundUser) {
             throw new Error('User not found');
         }
-        const isMatch = await bcrypt.compare(user.password, foundUser.password);
+        const isMatch = await bcrypt.compare(password, foundUser.password);
+        
         if (!isMatch) {
             throw new Error('Wrong password');
         }
+        
         const token = jwt.sign({ _id: foundUser._id }, config.jwt.secret, { expiresIn: config.jwt.expiration });
         return token;
     } catch (error) {
@@ -33,7 +48,67 @@ const login = async (user) => {
     }
 }
 
+const bookTable = async (booking) => {
+    try {
+        const newBooking = await bookingService.createBooking(booking);
+        return newBooking;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const viewMenu = async () => {
+    try {
+        const meals = await mealService.getAllMeals();
+        return meals;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const getUser = async (id) => {
+    try {
+        const user = await userRepo.findUserById(id);
+        return user;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const getUsers = async () => {
+    try {
+        const users = await userRepo.findAllUsers();
+        return users;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const updateUser = async (id, user) => {
+    try {
+        const updatedUser = await userRepo.updateUser(id, user);
+        return updatedUser;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const getBookings = async (id) => {
+    try {
+        const bookings = await bookingService.getAllUserBookings(id);
+        return bookings;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
 module.exports = {
     signUp,
-    login
+    login,
+    bookTable,
+    viewMenu,
+    getUser,
+    getUsers,
+    updateUser,
+    getBookings
 }
