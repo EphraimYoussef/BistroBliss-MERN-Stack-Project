4b595084d918ed76950d16a599153a517b9d6819
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-08 17:25:21 +0200

    Update App

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
-const config = require("./config/config.js");
-const connectDB = require("./config/db.js");
-
+const config = require('./config/config');
+const db = require('./config/db');
+const connectDB = require('./config/db');
+const authRouter = require('./routers/authRouter');
+const mealRouter = require('./routers/mealRouter');
+const userRouter = require('./routers/userRouter');
+const bookingRouter = require('./routers/bookingRouter');
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/menu', mealRouter);
+app.use('/api/bookings', bookingRouter);
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+const port = config.port || 5000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 })
