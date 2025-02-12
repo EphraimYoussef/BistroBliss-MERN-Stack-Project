603b28e3d69a606091cc2bb2ccfb5c281eb4cee9
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-12 20:04:06 +0200

    Update authController.js

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -4,38 +4,47 @@ const authServices = require('../services/authServices');
 
 
 const signup = async (req, res,next) => {
-    try{
-        const error = validationResult(req)
-        if(!error.isEmpty())
-            return res.status(400).json(error.array())
-        
-        const userDto = req.body
-        const {user,token} = await authServices.signUp(userDto)
-        res.cookie("token", token, { httpOnly: true }); 
-        res.json(jsend.success({user,token}))
-        return {user,token}
-    }catch(error){
-        next(error) 
-    }
+	try{
+			const error = validationResult(req)
+			console.log("heeyyy");
+			console.log(error);
+			
+			
+			if(!error.isEmpty())
+					return res.status(400).json(error.array())
+			
+			const userDto = req.body
+			
+			const {user,token} = await authServices.signUp(userDto)
+			res.cookie("token", token, { httpOnly: true }); 
+			res.json(jsend.success({user,token}))
+			return {user,token}
+	}catch(error){
+			next(error) 
+	}
 }
 
 const login = async (req, res,next) => {   
-    try{
-        const error = validationResult(req)
-        if(!error.isEmpty()) {
-            return res.status(400).json(error.array())
-          }
-        const {email, password} = req.body
-        const token = await authServices.login(email, password)
-        res.cookie("token", token, { httpOnly: true }); 
-        res.json(jsend.success({token}))
-        return token
-    }catch(error){
-        next(error) 
-    }
+	try{
+			const error = validationResult(req)
+			if(!error.isEmpty())
+						return res.status(400).json(error.array())
+			const {email, password} = req.body
+			const token = await authServices.login(email, password)
+			res.cookie("token", token, { httpOnly: true }); 
+			return res.json(jsend.success({token}))
+	}catch(error){
+			next(error) 
+	}
+}
+
+const logout = (req, res) => {
+	res.clearCookie("token");
+	res.json(jsend.success({message:"Logout Successfully"}))
 }
 
 module.exports = {
-    signup,
-    login
+	signup,
+	login,
+	logout
 }
