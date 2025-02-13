20e86e6b2d61d0ec86f038ebb790ee0904410b47
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-13 23:38:21 +0200

    Register V0.2.0

--- Client/src/components/pages/register/RegisterForm.jsx
+++ Client/src/components/pages/register/RegisterForm.jsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Checkbox } from "@/components/ui/checkbox"
 
 export default function LoginForm() {
   const [name, setName] = useState('')
@@ -17,10 +18,12 @@ export default function LoginForm() {
   }
 
   return (
-    <div className='bg-[#F9F9F7] min-h-screen flex flex-col justify-around items-center'>
+    <div className='bg-[#F9F9F7] min-h-screen flex flex-col justify-around items-center p-6'>
       <img src="Register.svg" alt="" className='p-5'/>
-      <div className="w-full max-w-2xl mx-auto p-10 m-10 bg-white rounded-xl shadow-lg">
-        <form onSubmit={handleSubmit} className="space-y-6 flex flex-col gap-5">
+      <div className="w-full max-w-2xl mx-5 p-10 m-10 bg-white rounded-xl shadow-lg">
+        <form onSubmit={handleSubmit} className="space-y-3 flex flex-col gap-5">
+
+          {/* Name */}
           <div className="space-y-2">
             <label htmlFor="name" className="block text-base font-medium text-gray-700">
               Name
@@ -34,6 +37,8 @@ export default function LoginForm() {
               className="w-full px-4 py-2 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
+
+          {/* Email */}
           <div className="space-y-2">
             <label htmlFor="name" className="block text-base font-medium text-gray-700">
               Email
@@ -47,6 +52,8 @@ export default function LoginForm() {
               className="w-full px-4 py-2 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
+
+          {/* Password */}
           <div className="space-y-2">
             <label htmlFor="name" className="block text-base font-medium text-gray-700">
               Password
@@ -60,6 +67,8 @@ export default function LoginForm() {
               className="w-full px-4 py-2 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
+
+          {/* Confirm */}
           <div className="space-y-2">
             <label htmlFor="name" className="block text-base font-medium text-gray-700">
               Confirm Password
@@ -73,12 +82,26 @@ export default function LoginForm() {
               className="w-full px-4 py-2 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
+
+          {/* Admin */}
+          <div className="flex items-center space-x-2">
+            <Checkbox id="Admin" className="text-white"/>
+            <label
+              htmlFor = "Admin"
+              className = "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Register as Admin.
+            </label>
+          </div>
+
+          {/* Register Button */}
           <Button
             type="submit"
             className="w-full px-4 py-3 font-semibold rounded-full text-white"
           >
             Register
           </Button>
+
         </form>
       </div>
     </div>
