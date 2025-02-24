1aeb940a2f90e49e09fb7b9030df6fb1c5838257
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-24 12:07:22 +0200

    Error Handling

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,8 @@ const authRouter = require('./routers/authRouter');
 const userRouter = require('./routers/userRouter');
 const mealRouter = require('./routers/mealRouter');
 const bookingRouter = require('./routers/bookingRouter');
+const { error } = require('jsend');
+const { NotFound } = require('./errors/CustomErrors');
 
 const app = express();
 
@@ -22,6 +24,22 @@ app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/menu', mealRouter);
 app.use('/api/bookings', bookingRouter);
+
+app.all('*', (req, res, next) => {
+    const err = new NotFound("Resource not found.");
+    next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'Internal Server Error';
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({ 
+        status: err.status, 
+        message: err.message
+    });
+});
 
 // Start Server
 const port = config.port || 5000;
