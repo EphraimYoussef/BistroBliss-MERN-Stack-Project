7ccee18153f3bf429c33fa0b57c65467283cca2d
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-25 19:17:04 +0200

    auth.js

---
+++ Client/src/lib/auth.js
@@ -0,0 +1,49 @@
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
+
+const getIsAdmin = () => {
+  const token = Cookies.get("token");
+  if (!token) {
+    return false;
+  }
+  try {
+    const { isAdmin } = jwtDecode(token);
+    return isAdmin;
+  } catch (error) {
+    console.error("Token decoding error",error);
+    return false;
+  }
+  
+}
+
+const getUsername = () => {
+  const token = Cookies.get("token");
+  if (!token) {
+    return null;
+  }
+  try {
+    const { username } = jwtDecode(token);
+    return username;
+  } catch (error) {
+    console.error("Token decoding error",error);
+    return null;
+  }
+  
+}
+
+const isTokenValid = () => {
+  const token = Cookies.get("token");
+  if (!token) {
+    return false;
+  }
+  try {
+    const { exp } = jwtDecode(token);
+    return (exp * 1000) > Date.now();
+  } catch (error) {
+    console.error("Token decoding error",error);
+    return false;
+  }
+  
+}
+
+export { getIsAdmin, getUsername, isTokenValid };
