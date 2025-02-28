fed279d5bd153576a4d2ab0e5068d976a607b8c1
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-02-28 10:54:21 +0200

    Update BookingValidator

--- Server/validators/bookingValidator.js
+++ Server/validators/bookingValidator.js
@@ -1,8 +1,7 @@
 const {body} = require('express-validator');
 
 const createBooking = [
-    body('username', 'Username is required').isString(),
-    body('email', 'email is required').isEmail(),
+    body('name', 'Username is required').isString(),
     body('phone', 'phone number is required to be the correct format (Egypt Code)').isMobilePhone("ar-EG"),
     body('date', 'date is required').isString(),
     body('time', 'time is required').isString(),
