8f744bbebecdf78fd9d976f7cc4a0aafc191eb02
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-03-01 13:03:05 +0200

    User MyBookings V1.0.0 ( integrated with backend )

--- Client/src/components/pages/profile/MyBookings.jsx
+++ Client/src/components/pages/profile/MyBookings.jsx
@@ -1,48 +1,51 @@
+"use client"
 import BookingItem from "@/atoms/BookingItem"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { getUserBookings } from "@/services/bookingServices"
+import { format, parse } from "date-fns"
 import { Playfair_Display } from 'next/font/google'
+import { useEffect, useState } from "react"
 
 const playFairDisplay_Font = Playfair_Display({ subsets: ['latin'] })
 
 const MyBookings = () => {
-    return (
-        <Card>
-            <CardHeader>
-              <CardTitle  className={ playFairDisplay_Font.className } >
-                <span className="font-semibold tracking-wide" >My Bookings</span>
-              </CardTitle>
-              <CardDescription>View and manage your current bookings.</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
+  const [booking, setBooking] = useState([])
+  useEffect(() => {
+    const getBookings = async () => {
+      const data = await getUserBookings();
+      if (data) {
+        setBooking(data);
+      }
+    }
+    getBookings();
+  },[])
+
+  return (
+    <Card>
+        <CardHeader>
+          <CardTitle  className={ playFairDisplay_Font.className } >
+            <span className="font-semibold tracking-wide" >My Bookings</span>
+          </CardTitle>
+          <CardDescription>View and manage your current bookings.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            {
+              booking.map((booking) => (
                 <BookingItem 
-                name="EVRA" 
-                date="March 15, 2025" 
-                time="8:00 PM"
-                totalPerson="5+"
-                phone="+201551220618"
-                email="evra@gmail.com"
-                status="Accepted" />
-                <BookingItem 
-                name="EVRA" 
-                date="May 15, 2025" 
-                time="5:00 PM"
-                totalPerson="1"
-                phone="+201551220618"
-                email="evra@gmail.com"
-                status="Rejected" />
-                <BookingItem 
-                name="EVRA" 
-                date="June 15, 2025" 
-                time="3:00 PM"
-                totalPerson="3"
-                phone="+201551220618"
-                email="evra@gmail.com"
-                status="Pending" />
-              </div>
-            </CardContent>
-          </Card>
-    )
+                  key={booking._id}
+                  name={booking.name} 
+                  date={format(new Date(booking.date), "PPP")} 
+                  time={format(parse(booking.time, "HH:mm", new Date()), "hh:mm a")}
+                  totalPerson={booking.totalPerson}
+                  phone={booking.phone}
+                  status={booking.status} />
+              ))
+            }
+          </div>
+        </CardContent>
+    </Card>
+  )
 }
 
 export default MyBookings
