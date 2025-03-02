896226ab58c807f604b674b4100ae2b7bd0c42ab
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-03-02 19:44:44 +0200

    Update bookingServices CLIENT

--- Client/src/services/bookingServices.js
+++ Client/src/services/bookingServices.js
@@ -66,7 +66,68 @@ const getUserBookings = async () => {
   }
 }
 
+const getPendingBookings = async () => {
+  if (!isTokenValid()) {
+    await logout();
+    window.location.href = '/';
+    return null;
+  }
+  try {
+    const response = await fetch(`${API_URL}/bookings`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Cookies.get('token')}`
+      }
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message || 'Something went wrong. Please try again.');
+    }
+
+    return await result.data.bookings;
+  } catch (error) {
+    throw new Error(`Could not get pending bookings: ${error.message}`);
+  }
+}
+
+const updateBookingStatus = async (id, status) => {
+  if (!isTokenValid()) {
+    await logout();
+    window.location.href = '/';
+    return null;
+  }
+  try {
+    const response = await fetch(`${API_URL}/bookings/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Cookies.get('token')}`
+      },
+      body: JSON.stringify({
+        status: status
+      })
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message || 'Something went wrong. Please try again.');
+    }
+
+    setInterval(() => {
+      window.location.reload();
+    }, 2500);
+
+    return await result.data.updatedBooking;
+  } catch (error) {
+    throw new Error(`Could not update booking status: ${error.message}`);
+  }
+}
+
 export{
   createBooking,
-  getUserBookings
+  getUserBookings,
+  getPendingBookings,
+  updateBookingStatus
 }
