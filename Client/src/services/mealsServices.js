e9e231195fa2d4bfff690c42d1fbb4988099d5f1
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-03-03 15:22:34 +0200

    Update mealsServices CLIENT

--- Client/src/services/mealsServices.js
+++ Client/src/services/mealsServices.js
@@ -1,3 +1,46 @@
+import { isTokenValid } from "@/lib/auth";
 import Cookies from "js-cookie";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const addMeal = async (data) => {
+  if (!isTokenValid()) {
+    await logout();
+    window.location.href = '/';
+    return null;
+  }
+  try {
+    const response = await fetch(`${API_URL}/meals`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Cookies.get('token')}`
+      },
+      body: JSON.stringify({
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        category: data.category,
+        image: data.image
+      })
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message || 'Something went wrong. Please try again.');
+    }
+
+    setInterval(() => {
+      window.location.reload();
+    }, 2500);
+
+    return await result.data.meal;
+  }catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+
+export { 
+  addMeal,
+};
