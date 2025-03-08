83185b7d48093879b54394bbdcb2463c474c5b6c
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-03-08 22:04:39 +0200

    Admin PendingBookings V1.1.0

--- Client/src/components/pages/admin/PendingList.jsx
+++ Client/src/components/pages/admin/PendingList.jsx
@@ -5,20 +5,27 @@ import { Playfair_Display } from "next/font/google"
 import PendingItem from "@/atoms/PendingITem"
 import { getPendingBookings } from "@/services/bookingServices"
 import { format, parse } from "date-fns"
+import { Skeleton } from "@/components/ui/skeleton"
+import EmptyPending from "./EmptyPending"
 
 const playFairDisplay_Font = Playfair_Display({ 
   subsets: ["latin"],
   style: "italic",
+  weight: "700"
 })
+
 const PendingList = () => {
   const [bookings, setBookings] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchBookings = async () => {
+      setLoading(true);
       const data = await getPendingBookings();
       if (data) {
         setBookings(data);
       }
+      setLoading(false);
     }
 
     fetchBookings();
@@ -33,20 +40,36 @@ const PendingList = () => {
       </div>
 
       <div className="border-t border-gray-200">
-        <ul className=" flex flex-col gap-5 py-5" >
-          {bookings.map((booking) => (
-            <li key={booking._id} className="px-4  sm:px-6">
-              <PendingItem 
-                key={booking._id} 
-                bookingId={booking._id}
-                name={booking.name} 
-                date={format(new Date(booking.date), "PPP")} 
-                time={format(parse(booking.time, "HH:mm", new Date()), "hh:mm a")} 
-                status={booking.status} phone={booking.phone} 
-                totalPerson={booking.capacity} />
-            </li>
-          ))}
-        </ul>
+        {
+          loading ?
+            <div className="flex flex-col divide-gray-200 py-5 px-6 gap-5">
+              {[...Array(3)].map((_, index) => (
+                <div key={index} className="flex flex-col gap-4 p-3 border rounded-lg ">
+                  <Skeleton className="h-7 w-full" />
+                  <Skeleton className="h-7 w-full" />
+                  <Skeleton className="h-7 w-full" />
+                </div>
+              )) } 
+            </div>
+            :
+              bookings.length == 0 ?
+                <EmptyPending />
+              :
+                <ul className=" flex flex-col gap-5 py-5" >
+                  {bookings.map((booking) => (
+                    <li key={booking._id} className="px-4  sm:px-6">
+                      <PendingItem 
+                        key={booking._id} 
+                        bookingId={booking._id}
+                        name={booking.name} 
+                        date={format(new Date(booking.date), "PPP")} 
+                        time={format(parse(booking.time, "HH:mm", new Date()), "hh:mm a")} 
+                        status={booking.status} phone={booking.phone} 
+                        totalPerson={booking.capacity} />
+                    </li>
+                  ))}
+                </ul>
+        }
       </div>
 
     </div>
