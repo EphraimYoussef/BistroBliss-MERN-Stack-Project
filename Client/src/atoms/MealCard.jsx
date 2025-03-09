ec0e7653fbc1e717a7ac7c8956877ec174f5e593
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-03-10 00:25:03 +0200

    MealCard V1.0.0

--- Client/src/atoms/MealCard.jsx
+++ Client/src/atoms/MealCard.jsx
@@ -1,19 +1,52 @@
-import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
-const MealCard = () => {
-	return (
-		<Card className='max-w-xs mx-auto rounded-3xl shadow-md hover:shadow-lg transition-shadow duration-300 bg-[#F9F9F7] overflow-hidden'>
-			<CardContent className='flex flex-col items-center gap-6 h-[400px] p-0'>
-				<img src="https://thispersondoesnotexist.com/" alt="" className='h-1/2 w-full'/>
-				<section className='flex items-center flex-col h-1/2 gap-5'>
-					<CardTitle className="text-primary">$15.99</CardTitle>
-					<CardTitle>Card Title</CardTitle>
-					<CardDescription className='text-center'>Made with eggs, lettuce, salt, oil and other ingredients.</CardDescription>
-				</section>
-			</CardContent>
-		</Card>
-	)
+const MealCard = ({ meal }) => {
+  const isLongDescription = meal.description.length > 75;
+
+  return (
+    <Card className='w-80 mx-auto rounded-3xl shadow-md hover:shadow-lg 
+      transition-shadow duration-300 bg-[#F9F9F7] overflow-hidden'>
+      <CardContent className='flex flex-col items-center gap-4 h-[450px] p-0'>
+        <img src={meal.image} alt="" className='h-1/2 w-full'/>
+        <section className='flex items-center flex-col h-1/2 gap-5'>
+          <CardTitle className="text-primary">{meal.price} E£</CardTitle>
+          <CardTitle>{meal.name}</CardTitle>
+          <CardTitle className='text-primary-dark text-base'>{meal.category}</CardTitle>
+          <div className="w-full">
+            <CardDescription className="text-center line-clamp-2 px-3">
+              {meal.description.slice(0, 75)}{isLongDescription && "..."}
+            </CardDescription>
+            {
+							isLongDescription && (
+								<Dialog >
+									<DialogTrigger asChild>
+										<Button variant="link" size="sm" className="mt-1 h-auto p-0 mx-auto block">
+											Read full description
+										</Button>
+									</DialogTrigger>
+									<DialogContent className="bg-[#F9F9F7]">
+										<DialogHeader>
+											<DialogTitle>{meal.name}</DialogTitle>
+											<DialogDescription className="pt-4">{meal.description}</DialogDescription>
+										</DialogHeader>
+									</DialogContent>
+								</Dialog>
+            )}
+          </div>
+        </section>
+      </CardContent>
+    </Card>
+  )
 }
 
-export default MealCard
+export default MealCard;
